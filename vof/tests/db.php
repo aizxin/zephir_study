adeeb6e385d7f397900ea72138016dd9d8f0e89f
aizxin <aizxin@gmail.com>
2017-08-16 01:52:49 +0800

    wang.orm

--- vof/tests/db.php
+++ vof/tests/db.php
@@ -1,7 +1,7 @@
 <?php
 
-use Vof\Medoo;
-
+// use Vof\Medoo;
+use Medoo\Medoo;
 $data = [
     'database_type' => 'mysql',
     'database_name' => 'kamun',
@@ -13,24 +13,24 @@ $data = [
     'charset' => 'utf8',
     'port' => 3306,
 
-    // [optional] Table prefix
-    // 'prefix' => 'PREFIX_',
+    // // [optional] Table prefix
+    // // 'prefix' => 'PREFIX_',
 
-    // [optional] Enable logging (Logging is disabled by default for better performance)
-    'logging' => true,
+    // // [optional] Enable logging (Logging is disabled by default for better performance)
+    // 'logging' => true,
 
-    // [optional] MySQL socket (shouldn't be used with server and port)
-    // 'socket' => '/home/mysql.sock',
+    // // [optional] MySQL socket (shouldn't be used with server and port)
+    // // 'socket' => '/home/mysql.sock',
 
-    // [optional] driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
-    'option' => [
-        PDO::ATTR_CASE => PDO::CASE_NATURAL
-    ],
+    // // [optional] driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
+    // 'option' => [
+    //     PDO::ATTR_CASE => PDO::CASE_NATURAL
+    // ],
 
-    // [optional] Medoo will execute those commands after connected to the database for initialization
-    'command' => [
-        'SET SQL_MODE=ANSI_QUOTES'
-    ]
+    // // [optional] Medoo will execute those commands after connected to the database for initialization
+    // 'command' => [
+    //     'SET SQL_MODE=ANSI_QUOTES'
+    // ]
 ];
 $database = new Medoo($data);
 
